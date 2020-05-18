17ab7045662ef42fe50f15f7e4e3901e3eb5e371
Ben <bzlister@gmail.com>
2020-05-18 01:06:05 -0400

    minizing pixelwise differences, maximizing classification loss

--- pix2pix.py
+++ pix2pix.py
@@ -23,16 +23,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-from fastai.vision import ImageList, LabelSmoothingCrossEntropy, Path, get_transforms, imagenet_stats, Learner, accuracy
-from efficientnet_pytorch import EfficientNet
-
+import pyramid
+from loss_pyramid import EdgeSaliencyLoss
+from fastai.vision import load_learner
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
     parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
     parser.add_argument("--dataset_name", type=str, default="birds", help="name of the dataset")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
@@ -51,18 +51,23 @@ if __name__ == '__main__':
     os.makedirs("saved_models/%s" % opt.dataset_name, exist_ok=True)
 
     cuda = True if torch.cuda.is_available() else False
-    device = torch.device('cuda')
+
+    lambda_clf = 10
+    lambda_pixel = 5
     # Loss functions
     criterion_GAN = torch.nn.MSELoss()
     #criterion_pixelwise = torch.nn.L1Loss()
-    criterion_ssim = pytorch_ssim.SSIM()
+    criterion_pixelwise = pytorch_ssim.SSIM()
     criterion_crossentropy = torch.nn.CrossEntropyLoss()
     criterion_hist = torch.nn.MSELoss()
+    softmax = torch.nn.Softmax()
 
-    # Loss weight of L1 pixel-wise loss between translated image and real image
-    lambda_pixel = -5
-    lambda_clf = 0.3
-    lambda_hist = 0.0000007
+    learn = load_learner('data/birds/')
+    learn.train_bn = False
+    clf = learn.model
+    for param in clf.parameters():
+        param.requires_grad = False
+    classes = ['ALBATROSS', 'ALEXANDRINE PARAKEET', 'AMERICAN AVOCET', 'AMERICAN BITTERN', 'AMERICAN COOT', 'AMERICAN GOLDFINCH', 'AMERICAN KESTREL', 'AMERICAN PIPIT', 'AMERICAN REDSTART', 'ANHINGA', 'ANNAS HUMMINGBIRD', 'ANTBIRD', 'ARARIPE MANAKIN', 'BALD EAGLE', 'BALTIMORE ORIOLE', 'BANANAQUIT', 'BAR-TAILED GODWIT', 'BARN OWL', 'BARN SWALLOW', 'BAY-BREASTED WARBLER', 'BELTED KINGFISHER', 'BIRD OF PARADISE', 'BLACK FRANCOLIN', 'BLACK SKIMMER', 'BLACK SWAN', 'BLACK THROATED WARBLER', 'BLACK VULTURE', 'BLACK-CAPPED CHICKADEE', 'BLACK-NECKED GREBE', 'BLACK-THROATED SPARROW', 'BLACKBURNIAM WARBLER', 'BLUE GROUSE', 'BLUE HERON', 'BOBOLINK', 'BROWN NOODY', 'BROWN THRASHER', 'CACTUS WREN', 'CALIFORNIA CONDOR', 'CALIFORNIA GULL', 'CALIFORNIA QUAIL', 'CANARY', 'CAPE MAY WARBLER', 'CARMINE BEE-EATER', 'CASPIAN TERN', 'CASSOWARY', 'CHARA DE COLLAR', 'CHIPPING SPARROW', 'CINNAMON TEAL', 'COCK OF THE  ROCK', 'COCKATOO', 'COMMON GRACKLE', 'COMMON HOUSE MARTIN', 'COMMON LOON', 'COMMON POORWILL', 'COMMON STARLING', 'COUCHS KINGBIRD', 'CRESTED AUKLET', 'CRESTED CARACARA', 'CROW', 'CROWNED PIGEON', 'CUBAN TODY', 'CURL CRESTED ARACURI', 'D-ARNAUDS BARBET', 'DARK EYED JUNCO', 'DOWNY WOODPECKER', 'EASTERN BLUEBIRD', 'EASTERN MEADOWLARK', 'EASTERN ROSELLA', 'EASTERN TOWEE', 'ELEGANT TROGON', 'ELLIOTS  PHEASANT', 'EMPEROR PENGUIN', 'EMU', 'EURASIAN MAGPIE', 'EVENING GROSBEAK', 'FLAME TANAGER', 'FLAMINGO', 'FRIGATE', 'GILA WOODPECKER', 'GLOSSY IBIS', 'GOLD WING WARBLER', 'GOLDEN CHLOROPHONIA', 'GOLDEN EAGLE', 'GOLDEN PHEASANT', 'GOULDIAN FINCH', 'GRAY CATBIRD', 'GRAY PARTRIDGE', 'GREEN JAY', 'GREY PLOVER', 'GUINEAFOWL', 'HAWAIIAN GOOSE', 'HOODED MERGANSER', 'HOOPOES', 'HORNBILL', 'HOUSE FINCH', 'HOUSE SPARROW', 'HYACINTH MACAW', 'IMPERIAL SHAQ', 'INCA TERN', 'INDIGO BUNTING', 'JABIRU', 'JAVAN MAGPIE', 'KILLDEAR', 'KING VULTURE', 'LARK BUNTING', 'LILAC ROLLER', 'LONG-EARED OWL', 'MALEO', 'MALLARD DUCK', 'MANDRIN DUCK', 'MARABOU STORK', 'MASKED BOOBY', 'MIKADO  PHEASANT', 'MOURNING DOVE', 'MYNA', 'NICOBAR PIGEON', 'NORTHERN CARDINAL', 'NORTHERN FLICKER', 'NORTHERN GANNET', 'NORTHERN GOSHAWK', 'NORTHERN JACANA', 'NORTHERN MOCKINGBIRD', 'NORTHERN RED BISHOP', 'OCELLATED TURKEY', 'OSPREY', 'OSTRICH', 'PAINTED BUNTIG', 'PARADISE TANAGER', 'PARUS MAJOR', 'PEACOCK', 'PELICAN', 'PEREGRINE FALCON', 'PINK ROBIN', 'PUFFIN', 'PURPLE FINCH', 'PURPLE GALLINULE', 'PURPLE MARTIN', 'PURPLE SWAMPHEN', 'QUETZAL', 'RAINBOW LORIKEET', 'RAZORBILL', 'RED BISHOP WEAVER', 'RED FACED CORMORANT', 'RED HEADED DUCK', 'RED HEADED WOODPECKER', 'RED HONEY CREEPER', 'RED THROATED BEE EATER', 'RED WINGED BLACKBIRD', 'RED WISKERED BULBUL', 'RING-BILLED GULL', 'RING-NECKED PHEASANT', 'ROADRUNNER', 'ROBIN', 'ROCK DOVE', 'ROSY FACED LOVEBIRD', 'ROUGH LEG BUZZARD', 'RUBY THROATED HUMMINGBIRD', 'RUFOUS KINGFISHER', 'RUFUOS MOTMOT', 'SAND MARTIN', 'SCARLET IBIS', 'SCARLET MACAW', 'SHOEBILL', 'SNOWY EGRET', 'SNOWY OWL', 'SORA', 'SPANGLED COTINGA', 'SPLENDID WREN', 'SPOONBILL', 'STEAMER DUCK', 'STORK BILLED KINGFISHER', 'STRAWBERRY FINCH', 'TAIWAN MAGPIE', 'TEAL DUCK', 'TIT MOUSE', 'TOUCHAN', 'TRUMPTER SWAN', 'TURKEY VULTURE', 'TURQUOISE MOTMOT', 'VARIED THRUSH', 'VENEZUELIAN TROUPIAL', 'VERMILION FLYCATHER', 'VIOLET GREEN SWALLOW', 'WHITE CHEEKED TURACO', 'WHITE NECKED RAVEN', 'WHITE TAILED TROPIC', 'WILD TURKEY', 'WILSONS BIRD OF PARADISE', 'WOOD DUCK', 'YELLOW HEADED BLACKBIRD']
 
     # Calculate output of image discriminator (PatchGAN)
     patch = (1, opt.img_height // 2 ** 4, opt.img_width // 2 ** 4)
@@ -70,44 +75,24 @@ if __name__ == '__main__':
     # Initialize generator and discriminator
     generator = GeneratorUNet()
     discriminator = Discriminator()
-    path = Path('C:/Users/bzlis/Documents/CS 747/bird-gan/data/birds/')
-    path.ls()
-    # Create the data using fastai's Datablock API
-    src = (ImageList.from_folder(path)
-                    .split_by_folder(train='train', valid='val')
-                    .label_from_folder()
-                    .add_test_folder('test')
-                    .transform(get_transforms(), size=224))
-
-    data = src.databunch(bs=32).normalize(imagenet_stats)
-
-    # Replace the fully connected layer at the end to fit our task
-    # Pre-trained model based on adversarial training
-    arch = EfficientNet.from_pretrained("efficientnet-b0", advprop=True)
-    arch._fc = nn.Linear(1280   , data.c)
-    # Define custom loss function
-    loss_func = LabelSmoothingCrossEntropy()
-
-    # Define the model
-    learn = Learner(data, arch, loss_func=loss_func, metrics=accuracy, model_dir='/kaggle/working')
-    clf = learn.model
 
     if cuda:
         generator = generator.cuda()
         discriminator = discriminator.cuda()
         criterion_crossentropy = criterion_crossentropy.cuda()
         criterion_GAN = criterion_GAN.cuda()
-        criterion_pixelwise = criterion_ssim.cuda()#criterion_pixelwise.cuda()
+        criterion_pixelwise = criterion_pixelwise.cuda()#criterion_pixelwise.cuda()
         criterion_hist = criterion_hist.cuda()
+        softmax = softmax.cuda()
+        device = torch.device('cuda')
 
-        clf.load_state_dict(torch.load('model.pth'))
-        clf.to(torch.device("cuda"))
+        #pyra = pyra.to(device)
         print("Models on GPU")
     else:
-        clf.load_state_dict(torch.load('model.pth', map_location=torch.device('cpu')))
-        clf.cuda()
+        device = torch.device('cpu')
         print("Models on CPU")
 
+    clf.to(device)
     if opt.epoch != 0:
         # Load pretrained models
         generator.load_state_dict(torch.load("saved_models/%s/generator_%d.pth" % (opt.dataset_name, opt.epoch)))
@@ -125,7 +110,7 @@ if __name__ == '__main__':
     transforms_ = [
         transforms.Resize((opt.img_height, opt.img_width), Image.BICUBIC),
         transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
@@ -152,9 +137,12 @@ if __name__ == '__main__':
         """Saves a generated sample from the validation set"""
         imgs = next(iter(val_dataloader))
         input = Variable(imgs["img"].type(Tensor))
+        correct_label = imgs["label"]
         output = generator(input)
+        pred = clf(output.to(device))
+        incorrect_label = (pred == pred.max()).nonzero()[0][1].item()
         img_sample = torch.cat((input.data, output.data), -2)
-        save_image(img_sample, "images/%s/%s.png" % (opt.dataset_name, batches_done), nrow=5, normalize=True)
+        save_image(img_sample, "images/%s/%s ACTUAL=%s PREDICTED=%s.png" % (opt.dataset_name, batches_done, classes[correct_label], classes[incorrect_label]), nrow=5, normalize=True)
 
 
     # ----------
@@ -162,7 +150,6 @@ if __name__ == '__main__':
     # ----------
 
     prev_time = time.time()
-
     for epoch in range(opt.epoch, opt.n_epochs):
         for i, batch in enumerate(dataloader):
 
@@ -189,24 +176,19 @@ if __name__ == '__main__':
             # GAN loss
             loss_GAN = criterion_GAN(pred_fake, valid)
 
-            # Classification loss
-            loss_clf = criterion_crossentropy(pred_class, label)
-            # Histogram loss
-            # For thhe component of loss responsible for forcing the generator to produce spatial transforms (currently pixelwise):
-                # Try an edge loss thing like in pyramid feature attention
-                # Read that one paper and see what they do to achieve spatial transforms
-            # SSIM seems really good for getting nice looking images - perhaps it shouldnt be used as a 'bad thing' to motivate spatial transforms?
-            # histogram color distance appears to be working correctly
-            gan_output_std = gan_output*0.5 + 0.5
-            input_std = input*0.5 + 0.5
-            r_out, g_out, b_out = gan_output_std[:,0,:,:], gan_output_std[:,1,:,:], gan_output_std[:,2,:,:]
-            r_in, g_in, b_in = input_std[:,0,:,:], input_std[:,1,:,:], input_std[:,2,:,:]
-            # Pixel-wise loss
-            loss_pixel = criterion_pixelwise((0.299*r_out + 0.587*g_out + 0.114*b_out).reshape(-1,1,256,256), (0.299*r_in + 0.587*g_in + 0.114*b_in).reshape(-1,1,256,256))**2
-            loss_hist = criterion_hist(r_out.histc(bins=256), r_in.histc(bins=256)).detach() + criterion_hist(g_out.histc(bins=256), g_in.histc(bins=256)).detach() + criterion_hist(b_out.histc(bins=256), b_in.histc(bins=256)).detach()
-            # Total loss
-            loss_G = loss_GAN + lambda_pixel * loss_pixel + lambda_clf * loss_clf + lambda_hist * loss_hist
 
+            loss_clf = 1/criterion_crossentropy(pred_class,label).detach()
+
+            '''
+            r_out, g_out, b_out = gan_output[:,0,:,:], gan_output[:,1,:,:], gan_output[:,2,:,:]
+            r_in, g_in, b_in = input[:,0,:,:], input[:,1,:,:], input[:,2,:,:]
+            loss_pixelwise = 1-criterion_pixelwise((0.299*r_out + 0.587*g_out + 0.114*b_out).reshape(-1,1,256,256), (0.299*r_in + 0.587*g_in + 0.114*b_in).reshape(-1,1,256,256))
+            loss_hist = criterion_hist(r_out.histc(bins=256), r_in.histc(bins=256)).detach() + criterion_hist(g_out.histc(bins=256), g_in.histc(bins=256)).detach() + criterion_hist(b_out.histc(bins=256), b_in.histc(bins=256)).detach()
+            '''
+            loss_pixelwise = 1 - criterion_pixelwise(gan_output, input)
+
+            # Total loss
+            loss_G = loss_GAN + lambda_pixel*loss_pixelwise + lambda_clf*loss_clf
             loss_G.backward()
 
             optimizer_G.step()
@@ -238,21 +220,33 @@ if __name__ == '__main__':
             prev_time = time.time()
             # Print log
             sys.stdout.write(
-                "\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, pixel: %f, clf: %f, hist: %f, adv: %f] ETA: %s"
+                "\r[Batch %d/%d] [D loss: %f] [G loss: %f, clf: %f, pixel: %f, adv: %f] ETA: %s"
                 % (
-                    epoch,
-                    opt.n_epochs,
                     i,
                     len(dataloader),
                     loss_D.item(),
                     loss_G.item(),
-                    loss_pixel.item()*lambda_pixel,
                     loss_clf.item()*lambda_clf,
-                    loss_hist.item()*lambda_hist,
+                    loss_pixelwise.item()*lambda_pixel,
                     loss_GAN.item(),
                     time_left,
                 )
             )
+            '''
+            sys.stdout.write(
+                "\r[Batch %d/%d] [D loss: %f] [G loss: %f, clf_right: %f, clf_wrong: %f, adv: %f] ETA: %s"
+                % (
+                    i,
+                    len(dataloader),
+                    loss_D.item(),
+                    loss_G.item(),
+                    loss_clf_correct.item()*lambda_correct,
+                    loss_clf_incorrect.item()*lambda_incorrect,
+                    loss_GAN.item(),
+                    time_left,
+                )
+            )
+            '''
 
             # If at sample interval save image
             if batches_done % opt.sample_interval == 0:
